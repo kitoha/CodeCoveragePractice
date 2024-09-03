1bd2ebeeebe1b5c750d8ef6bec40c118ccadfbb9
kitoha <kitoha@daum.net>
2024-09-03 23:40:23 +0900

    [master] "develop branch test"

---
+++ src/main/java/com/practice/codeCoverage/service/coverageTestService.java
@@ -0,0 +1,18 @@
+package com.practice.codeCoverage.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class coverageTestService {
+
+    public String test(int value){
+        int a= 1+value;
+
+        if(a==2){
+            return "Good!";
+        }else{
+            return "Failed";
+        }
+
+    }
+}
